4b20fdf67ec5e748ae132cb4fea33fda166eaac7
slevinson <slevinson@heed-global.com>
2019-07-04 15:29:23 +0300

    add ctor

--- src/main/java/com/flight/flightapp/baggage/BaggageCheckRequest.java
+++ src/main/java/com/flight/flightapp/baggage/BaggageCheckRequest.java
@@ -1,5 +1,6 @@
 package com.flight.flightapp.baggage;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class BaggageCheckRequest {
     private String destinationId;
     private String baggageId;
